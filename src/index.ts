8e936b7ee337d3b1860281f56ffe5dcd552de1c9
Mitchell Stave <mitchjohnston99@gmail.com>
2024-10-09 12:33:41 -0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { note } from "./note";
+const app = new Elysia()
+  .use(swagger())
+  .use(note)
+  .listen(8088);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}/`
 );
---
+++ src/note.ts
@@ -0,0 +1,63 @@
+import { Elysia, t } from "elysia";
+
+class Note {
+  constructor(public data: string[] = ['moonhalo']) { }
+
+  add(note: string) {
+    this.data.push(note);
+    return this.data
+  }
+
+  remove(index: number) {
+    return this.data.splice(index, 1);
+  }
+
+  update(index: number, note: string) {
+    this.data[index] = note;
+    return this.data;
+  }
+
+}
+
+export const note = new Elysia({ prefix: '/note' })
+  .decorate('note', new Note())
+  .onTransform(function log({ body, params, path, request: {method}}) {
+    console.log(`${method} ${path}`, { body, params });
+  })
+  .get('/', ({ note }) => note.data)
+  .put('/', ({ note, body: { data } }) => note.add(data), {
+    body: t.Object({
+      data: t.String()
+    })
+  })
+  .guard({
+    params: t.Object({
+      index: t.Number()
+    })
+  })
+  .get(
+    '/:index',
+    ({ note, params: { index }, error }) => {
+      return note.data[index] ?? error(404, 'Not Found :(')
+    }
+  )
+  .delete(
+    '/:index',
+    ({ note, params: { index }, error}) => {
+      if (index in note.data) return note.remove(index);
+      return error(422, 'Cant complete :(')
+    }
+  )
+  .patch(
+    `/:index`,
+    ({ note, params: { index }, body: { data }, error}) => {
+      if (index in note.data) return note.update(index, data)
+
+      return error(422, 'Cant complete :(')
+    },
+    {
+      body: t.Object({
+        data: t.String()
+      })
+    }
+  )
